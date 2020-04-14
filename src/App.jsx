c034a0154928cc7114fe9ec5abdb9009194b4c31
Louis <louis.fowler@gmail.com>
2020-04-14 16:08:49 +1200

    started work on checkItemHandler in app and SortedPage components

--- src/App.jsx
+++ src/App.jsx
@@ -3,78 +3,91 @@ import "./App.css";
 import TopBanner from "./TopBanner/TopBanner";
 import ItemSorter from "./ItemSorter/ItemSorter";
 import HelpUsForm from "./HelpUsForm/HelpUsForm";
-import SortedPage from "./SortedPage/SortedPage"
-import AddItemForm from "./AddItemForm/AddItemForm"
+import SortedPage from "./SortedPage/SortedPage";
+import AddItemForm from "./AddItemForm/AddItemForm";
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
       sortPage: "toSort",
-      list: [{item: "apple", type: "fruit/veg", isle: 0, id: 123}, {item: "vogels", type: "bread", isle: 10, id:32135}, {item: "oranges", type: "fruit/veg", isle: 0, id: 97472}, {item: "toothpaste", type: "personal hygiene", isle: 3, id: 578926}, {item: "toilet paper", type: "personal hygiene", isle: 6, id: 5732847}],
-		};
+      list: [
+        { item: "apple", type: "fruit/veg", isle: 0, id: 123 },
+        { item: "vogels", type: "bread", isle: 10, id: 32135 },
+        { item: "oranges", type: "fruit/veg", isle: 0, id: 97472 },
+        { item: "toothpaste", type: "personal hygiene", isle: 3, id: 578926 },
+        { item: "toilet paper", type: "personal hygiene", isle: 6, id: 847 },
+      ],
+    };
   }
-  
 
-  ChangePage = () => { 
+  ChangePage = () => {
     if (this.state.sortPage === "toSort") {
       this.setState({
-        sortPage: "sorted"
-      })
+        sortPage: "sorted",
+      });
     } else {
       this.setState({
-        sortPage: "toSort"
-      })
+        sortPage: "toSort",
+      });
     }
-  }
+  };
 
   getItemHandler = (childData) => {
-    childData.item === "" ? 
-      alert("No item in input") : 
-      this.setState(prevState => ({
-        list: [childData, ...prevState.list]
-      }))
-  }
+    childData.item === ""
+      ? alert("No item in input")
+      : this.setState((prevState) => ({
+          list: [childData, ...prevState.list],
+        }));
+  };
 
-  
   SortHandler = () => {
-    this.setState(prevState => {
-      const list = prevState.list.sort((a,b) =>
-      (a.isle > b.isle) ? 1 : -1) 
-      
-      const showHelpText = prevState.list.map(a => a.isle).includes(-1) 
+    this.setState((prevState) => {
+      const list = prevState.list.sort((a, b) => (a.isle > b.isle ? 1 : -1));
+
+      const showHelpText = prevState.list.map((a) => a.isle).includes(-1);
 
       return {
         list,
         showHelpText,
-        sortPage: "sorted"
+        sortPage: "sorted",
       };
-    })
-  }
+    });
+  };
 
-  deleteItemHandler = itemId => {
-    const list = this.state.list.filter(i => i.id !== itemId)
-    this.setState({list: list})
-  }
+  deleteItemHandler = (itemId) => {
+    const list = this.state.list.filter((i) => i.id !== itemId);
+    this.setState({ list: list });
+  };
 
-	render() {
-		return (
-			<div className="App">
-				<TopBanner/>
+  // checkItemHandler = (itemId) => {
+  //   const item = this.state.list.filter((i) => i.id === itemId);
+  //   const spreadItem = [...item];
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        <TopBanner />
         {this.state.sortPage === "toSort" ? (
           <div>
-            <AddItemForm getItemHandler={this.getItemHandler} list={this.state.list}/>
-            <ItemSorter list={this.state.list} click={this.SortHandler} delete={this.deleteItemHandler}/>
+            <AddItemForm
+              getItemHandler={this.getItemHandler}
+              list={this.state.list}
+            />
+            <ItemSorter
+              list={this.state.list}
+              click={this.SortHandler}
+              delete={this.deleteItemHandler}
+            />
           </div>
         ) : (
-          <SortedPage list={this.state.list} click={this.ChangePage}/>
+          <SortedPage list={this.state.list} click={this.ChangePage} />
         )}
-        {this.state.showHelpText && (
-          <HelpUsForm/>
-        )}
-			</div> 
-		);
-	}
+        {this.state.showHelpText && <HelpUsForm />}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/SortedPage/SortedPage.jsx
+++ src/SortedPage/SortedPage.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const SortedPage = (props) => {
-
-
   return (
     <div>
-      {props.list.map(list=>
+      {props.list.map((list) => (
         <div>
           <div className="sortedCard" key={list.id}>
-            <div className="sCardText">
-              <h2>Isle: {list.isle === -1 ? 'This item has no isle yet!' : list.isle === 0 ? "grocery/baking" : list.isle}</h2>
+            <div className={!("style" in list) ? "sCardText" : list.style}>
+              <h2>
+                Isle:{" "}
+                {list.isle === -1
+                  ? "This item has no isle yet!"
+                  : list.isle === 0
+                  ? "grocery/baking"
+                  : list.isle}
+              </h2>
               <h2>Item: {list.item}</h2>
             </div>
           </div>
-          <hr/>
+          <hr />
         </div>
-      )}
-      <button className="sortButton" onClick={props.click}>Add or delete</button>
+      ))}
+      <button className="sortButton" onClick={props.click}>
+        Add or delete
+      </button>
     </div>
   );
-}
+};
 
 export default SortedPage;
