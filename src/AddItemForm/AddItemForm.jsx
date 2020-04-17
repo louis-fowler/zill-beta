df9856348925d8f329c5ac4bd89a64465e0906ef
Louis <louis.fowler@gmail.com>
2020-04-17 16:21:52 +1200

    css update to proper scale. added scalability

--- src/AddItemForm/AddItemForm.jsx
+++ src/AddItemForm/AddItemForm.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const AddItemForm = (props) => {
-  const [data, setData] = useState(
-    {item: "", type: "", isle: -1, id: 0}
-  );
+  const [data, setData] = useState({ item: "", type: "", isle: -1, id: 0 });
 
   /* Simon example
     const [item, setItem] = useState("");
@@ -21,41 +19,59 @@ const AddItemForm = (props) => {
 
   const sendItemHandler = (e) => {
     e.preventDefault();
-    const id = Date.now()
-    const randomNumber = Math.floor(Math.random() * 11) + 0
-    props.getItemHandler({item: data.item, type: data.type, isle: randomNumber, id:id});
-    setData({item: "", type: "", isle: -1, id: 0})
-  }
+    const id = Date.now();
+    const randomNumber = Math.floor(Math.random() * 11) + 0;
+    props.getItemHandler({
+      item: data.item,
+      type: data.type,
+      isle: randomNumber,
+      id: id,
+    });
+    setData({ item: "", type: "", isle: -1, id: 0 });
+  };
 
-  const addItemHandler = e => {
-    const newData = {...data}
-    newData.item = e.target.value
-    setData(newData)
-  }
+  const addItemHandler = (e) => {
+    const newData = { ...data };
+    newData.item = e.target.value;
+    setData(newData);
+  };
 
-  const addTypeHandler = e => {
-    const newData = {...data};
+  const addTypeHandler = (e) => {
+    const newData = { ...data };
     newData.type = e.target.value;
-    setData(newData)
-  }
+    setData(newData);
+  };
 
   return (
     <div>
       <div className="formWrapper">
-      <h3>Add Item</h3>
-        <form action="">
-          <input name="item" type="text" placeholder="Item..." value={data.item } onChange={addItemHandler}/>
-          <select name="type" id="" value={data.type} onChange={addTypeHandler}>
-            <option value="fruit/veg">Fruit and Veg</option>
-            <option value="bread">Bread</option>
-            <option value="personal hygiene">Personal hygiene</option>
-            <option value="?">Dont know</option>
-          </select>
-          <button onClick={sendItemHandler}>Add</button>
-        </form>
+        <h3>Add Item</h3>
+        <div className="addItemForm">
+          <form action="">
+            <input
+              name="item"
+              type="text"
+              placeholder="Item..."
+              value={data.item}
+              onChange={addItemHandler}
+            />
+            <select
+              name="type"
+              id=""
+              value={data.type}
+              onChange={addTypeHandler}
+            >
+              <option value="fruit/veg">Fruit and Veg</option>
+              <option value="bread">Bread</option>
+              <option value="personal hygiene">Personal hygiene</option>
+              <option value="?">Dont know</option>
+            </select>
+            <button onClick={sendItemHandler}>Add</button>
+          </form>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default AddItemForm;
--- src/TopBanner/TopBanner.jsx
+++ src/TopBanner/TopBanner.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-
-const TopBanner = props => {
-	return (
-		<div className="TopBanner">
-            <h1>Zill Shopper</h1>
-            <p>Helping you to shop quicker</p>
-        </div>
-	);
+const TopBanner = (props) => {
+  return (
+    <div className="TopBanner">
+      <h1>Zill Shopper</h1>
+      <h3>Helping you to shop quicker</h3>
+    </div>
+  );
 };
 
 export default TopBanner;
