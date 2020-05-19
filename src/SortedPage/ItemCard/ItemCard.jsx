478137117e98184fa223d7536cfef83f52b7e130
Louis <louis.fowler@gmail.com>
2020-05-19 16:30:21 +1200

    start in ordered sorted page under isle title

---
+++ src/SortedPage/ItemCard/ItemCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function ItemCard(props) {
+  return (
+    <div>
+      {props.array.map((list, index) => {
+        return (
+          <div className="sortedCard" key={list.id}>
+            <div
+              onClick={() => props.toggleCheck(list.check, index)}
+              className={list.check === false ? "sCardText" : "sCardTextCheck"}>
+              <p>
+                Isle:{" "}
+                {list.isle === -1
+                  ? "This item has no isle yet!"
+                  : list.isle === 0
+                  ? "grocery / baking"
+                  : list.isle}
+              </p>
+              <p>Item: {list.item}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ItemCard;
--- src/SortedPage/SortedPage.jsx
+++ src/SortedPage/SortedPage.jsx
@@ -1,27 +1,35 @@
 import React from "react";
+import ItemCard from "./ItemCard/ItemCard";
 
 const SortedPage = props => {
+  let grocArray = props.list.filter(i => i.isle === 0);
+  let oneArray = props.list.filter(i => i.isle === 1);
+  let twoArray = props.list.filter(i => i.isle === 2);
+  let threeArray = props.list.filter(i => i.isle === 3);
+  let fourArray = props.list.filter(i => i.isle === 4);
+  let fiveArray = props.list.filter(i => i.isle === 5);
+  let sixArray = props.list.filter(i => i.isle === 6);
+  let sevenArray = props.list.filter(i => i.isle === 7);
+  let eightArray = props.list.filter(i => i.isle === 8);
+  let nineArray = props.list.filter(i => i.isle === 9);
+  let tenArray = props.list.filter(i => i.isle === 10);
+
   return (
     <div>
-      {props.list.map((list, index) => (
-        <div className="sortedItemContainer">
-          <div className="sortedCard" key={list.id}>
-            <div
-              onClick={() => props.toggleCheck(list.check, index)}
-              className={list.check === false ? "sCardText" : "sCardTextCheck"}>
-              <p>
-                Isle:{" "}
-                {list.isle === -1
-                  ? "This item has no isle yet!"
-                  : list.isle === 0
-                  ? "grocery/baking"
-                  : list.isle}
-              </p>
-              <p>Item: {list.item}</p>
-            </div>
-          </div>
+      <h2>Click to check/un-check</h2>
+
+      {grocArray !== undefined || grocArray.length !== 0 ? (
+        <div>
+          <h3>Grocery and Baking Isle</h3>
+          <ItemCard array={grocArray} toggleCheck={props.toggleCheck} />
         </div>
-      ))}
+      ) : oneArray !== undefined || grocArray.length !== 0 ? (
+        <div>
+          <h3>Isle 1</h3>
+          <ItemCard array={oneArray} toggleCheck={props.toggleCheck} />
+        </div>
+      ) : null}
+
       <button className="sortButton" onClick={props.click}>
         Add or delete
       </button>
